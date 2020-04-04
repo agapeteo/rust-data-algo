15f8dc69e1c2f7f210b24ec3cc6cfb08e4e43209
Alex Gandzha <agapeteo@gmail.com>
2020-04-03 20:37:07 -0400

    LRU cache - lifetime rename

--- src/hashmap/lru_cache.rs
+++ src/hashmap/lru_cache.rs
@@ -1,19 +1,19 @@
 use std::collections::{HashMap, VecDeque};
 use std::hash::Hash;
 
-pub struct CacheLru<'a, 'b, K: Eq + Hash, V> {
-    cache: HashMap<&'a K, &'b V>,
-    queue: VecDeque<&'a K>,
+pub struct CacheLru<'k, 'v, K: Eq + Hash, V> {
+    cache: HashMap<&'k K, &'v V>,
+    queue: VecDeque<&'k K>,
     capacity: usize,
 }
 
 #[allow(dead_code)]
-impl<'a, 'b, K: Eq + Hash, V> CacheLru<'a, 'b, K, V> {
+impl<'k, 'v, K: Eq + Hash, V> CacheLru<'k, 'v, K, V> {
     pub fn new(capacity: usize) -> Self {
         CacheLru { cache: HashMap::new(), queue: VecDeque::new(), capacity }
     }
 
-    pub fn set(&mut self, key: &'a K, value: &'b V) {
+    pub fn set(&mut self, key: &'k K, value: &'v V) {
         if self.cache.len() >= self.capacity {
             let key_to_remove = self.queue.pop_front().unwrap();
             self.cache.remove_entry(key_to_remove);
@@ -22,7 +22,7 @@ impl<'a, 'b, K: Eq + Hash, V> CacheLru<'a, 'b, K, V> {
         self.queue.push_back(key);
     }
 
-    pub fn get(&self, key: &'a K) -> Option<&'b V> {
+    pub fn get(&self, key: &'k K) -> Option<&'v V> {
         self.cache.get(key).copied()
     }
 }
